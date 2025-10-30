d8555be31eb603dbddcc4463b13d7db5231cec47
Giovana Albanês <albanesgiovana@gmail.com>
2025-10-30 18:52:51 -0300

    feat: add Swagger para doc

--- src/main/java/com/atividades/atividade_2/controller/DisciplinesController.java
+++ src/main/java/com/atividades/atividade_2/controller/DisciplinesController.java
@@ -12,8 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/disciplines")
+@Tag(name = "Disciplines", description = "API para disciplinas de DSM")    
 public class DisciplinesController {
 
     private final Map<Integer, Map<String,String>> disciplinesBD = new HashMap<>();
@@ -24,18 +30,25 @@ public class DisciplinesController {
     }
 
     // Lista todas as disciplinas
+    @Operation(summary = "Lista todas as disciplinas", description = "Retorna uma lista com todas as disciplinas cadastradas")
+    @ApiResponse(responseCode = "200", description = "Lista de disciplinas retornada com sucesso")
     @GetMapping
     public List<Map<String, String>> getAllDisciplines() {
         return new ArrayList<>(disciplinesBD.values());
     }
 
     // Lista a disciplina pelo id
+    @Operation(summary = "Obtém uma disciplina pelo ID", description = "Retorna os detalhes de uma disciplina específica com base no ID fornecido")
+    @ApiResponse(responseCode = "200", description = "Disciplina retornada com sucesso")
+    @Parameter(name = "id", description = "ID da disciplina a ser obtida", required = true)
     @GetMapping("/{id}")
     public Map<String, String> getDisciplineById(@PathVariable int id){
         return disciplinesBD.get(id);
     }
 
     // Cria uma nova disciplina
+    @Operation(summary = "Cria uma nova disciplina", description = "Adiciona uma nova disciplina ao sistema com os detalhes fornecidos")
+    @ApiResponse(responseCode = "201", description = "Disciplina criada com sucesso")
     @PostMapping
     public Map<String, String> createDiscipline(@RequestBody Map<String, String> discipline) {
         Map<String, String> newDiscipline = new HashMap<>();
